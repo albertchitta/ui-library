79e17e27c4e890d17cc760664a70975542f04876
albertchitta <albert.w.chittaphong@gmail.com>
2024-06-23 16:08:58 -0500

    removed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import * as React from "react";
+import React from "react";
 import { colorVariant, conditionStyle, flexCenter, localVarStyle } from "./App.css";
 import { light, dark } from "./vars.css";
-import { Button } from "@/components/ui/button"
-
-export function ButtonDemo() {
-  return <Button>Button</Button>
-}
-
 
 function App() {
   return (
-    // <div className={light}>
-    //   <div className={localVarStyle}>
-    //     Hello World
-    //   </div>
-    //   <StyledComponent color="secondary"/>
-    //   <div className={conditionStyle}>
-    //     Test
-    //   </div>
-    // </div>
+    <div className={light}>
+      <div className={localVarStyle}>
+        Hello World
+      </div>
+      <StyledComponent color="secondary"/>
+      <div className={conditionStyle}>
+        Test
+      </div>
+    </div>
   )
 }
 
